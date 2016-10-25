65c608d0c95a24c13b7e23e0adc222e4e244cdae
KSean12 <KSeanUn@gmail.com>
2016-10-25 11:08:50 +0300

    Update Plugin.cs

--- DeathManager/Plugin.cs
+++ DeathManager/Plugin.cs
@@ -1,48 +1,19 @@
-﻿using fr34kyn01535.Uconomy;
-using Rocket.API;
 using Rocket.API.Collections;
-using Rocket.Core.Commands;
 using Rocket.Core.Logging;
 using Rocket.Core.Plugins;
 using Rocket.Unturned.Chat;
 using Rocket.Unturned.Events;
 using Rocket.Unturned.Player;
 using SDG.Unturned;
-using Steamworks;
 using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace DeathManager
 {
     public class DeathManager : RocketPlugin<Configuration>
     {
-        public static UnturnedPlayer killer;
         public static DeathManager Instance;
-        private IDictionary<string, DeathNote> deathNotes;
 
-
-        protected override void Load()
-        {
-            Instance = this;
-
-            if (Instance.Configuration.Instance.Enabled)
-            {
-                deathNotes = new Dictionary<string, DeathNote>();
-                UnturnedPlayerEvents.OnPlayerDeath += UnturnedPlayerEvents_OnPlayerDeath;
-
-                Logger.Log("[DeathManager] The Plugin Has Been loaded!");
-                
-            }
-        }
-
-        protected override void Unload()
-        {
-            if (Instance.Configuration.Instance.Enabled)
-            {
-                UnturnedPlayerEvents.OnPlayerDeath -= UnturnedPlayerEvents_OnPlayerDeath;
-            }
-        }
         public override TranslationList DefaultTranslations
         {
             get
@@ -50,7 +21,7 @@ namespace DeathManager
                 return new TranslationList()
                 {
                     {"gun","{0} [GUN] {2} {1}"},
-                    {"death","ADMIN [KILL] {0}"},
+                    {"kill","ADMIN [KILL] {0}"},
                     {"null","[NULL] {0}"},
                     {"food","[FOOD] {0}"},
                     {"arena","[ARENA] {0}"},
@@ -75,28 +46,53 @@ namespace DeathManager
                 };
             }
         }
-
-        private void UnturnedPlayerEvents_OnPlayerDeath(UnturnedPlayer player, EDeathCause cause, ELimb limb, CSteamID murderer )
+        protected override void Load()
         {
-            deathNotes[player.SteamName] = new DeathNote() { Position = player.Position, TimeOfDeath = DateTime.Now };
-            killer = UnturnedPlayer.FromCSteamID(murderer);
+            Instance = this;
+
+            if (Instance.Configuration.Instance.Enabled)
+            {
+                UnturnedPlayerEvents.OnPlayerDeath += OnPlayerDeath;
+                Logger.LogWarning("The plugin has been Loaded!");
+                if (Instance.Configuration.Instance.ShowErrorMessages)
+                {
+                    Logger.LogWarning("| Show error messages : True                          |");
+                }
+                else
+                {
+                    Logger.LogWarning("| Show error messages : False                         |");
+                }
+                if (Instance.Configuration.Instance.ShowSuicideMessages)
+                {
+                    Logger.LogWarning("| Show suicide messages : True                        |");
+                }
+                else
+                {
+                    Logger.LogWarning("| Show suicide messages : False                       |");
+                }
+            }
+        }
+
+        protected override void Unload()
+        {
+            if (Instance.Configuration.Instance.Enabled)
+            {
+                UnturnedPlayerEvents.OnPlayerDeath -= OnPlayerDeath;
+            }
+        }
+
+        private void OnPlayerDeath(UnturnedPlayer player, EDeathCause cause, ELimb limb, Steamworks.CSteamID murderer)
+        {
+            UnturnedPlayer killer = UnturnedPlayer.FromCSteamID(murderer);
+
             string death = cause.ToString();
             string headshot = string.Empty;
-            string Received = Configuration.Instance.KillerReward + Uconomy.Instance.Configuration.Instance.MoneyName;
-            string TotalMoney = Uconomy.Instance.Database.IncreaseBalance(killer.Id, Configuration.Instance.KillerReward) + Uconomy.Instance.Configuration.Instance.MoneyName;
-            if (Instance.Configuration.Instance.EffectOnDeath)
-            {
-                EffectManager.sendEffect((ushort)Instance.Configuration.Instance.EffectOnDeathId, 30, player.Position);
-            }
-            if (Instance.Configuration.Instance.ShowHeadshotMessages)
-            {
-                headshot = Instance.Translations.Instance.Translate("headshot");
-            }
+            if (Instance.Configuration.Instance.ShowHeadshotMessages) { headshot = Instance.Translations.Instance.Translate("headshot"); }
+
             try
             {
                 switch (death)
                 {
-
                     case "BLEEDING":
                         UnturnedChat.Say(Translate("bleeding", player.DisplayName), UnturnedChat.GetColorFromName(Configuration.Instance.DeathMessagesColor, Color.green));
                         break;
@@ -173,64 +169,17 @@ namespace DeathManager
                     default:
                         UnturnedChat.Say(Translate("null", player.DisplayName), UnturnedChat.GetColorFromName(Configuration.Instance.DeathMessagesColor, Color.green));
                         break;
-
-
                 }
             }
-
             catch (Exception exc)
             {
-                if (Instance.Configuration.Instance.Debug)
+                if (Instance.Configuration.Instance.ShowErrorMessages)
                 {
-                    Logger.LogError("[DeathManager] Error : " + exc);
-
+                    Logger.LogError("Error: Could not find <cause.Death>");
+                    Logger.LogError("Description: " + exc);
                 }
-            }
-        }
-        internal void TrySendBack(UnturnedPlayer player )
-        {
-            if (!Configuration.Instance.BackEnabled)
-                return;
-
-            if (deathNotes.ContainsKey(player.SteamName))
-            {
-                DeathNote deathNote = deathNotes[player.SteamName];
-
-                if (DateTime.Now <= deathNote.TimeOfDeath.AddSeconds(Configuration.Instance.TimeLimit))
-                {
-                    // Teleporting the player back.
-                    player.Teleport(deathNote.Position, player.Rotation);
-
-                    // Saying hello to the dead :)
-                    UnturnedChat.Say(player, "Back !");
-                }
-                else
-                    UnturnedChat.Say(player, "too slow, have a nice walk. ");
-
-                // Removing the reference, we dont want a smart player using our plugin as fast travel.
-                deathNotes.Remove(player.SteamName);
-            }
-            else
-            {
-                // Cant find the poor guy =/
-                UnturnedChat.Say(player, "You have to die first !");
-            }
-        }
-        [RocketCommand("back", "Back To Death Location", "", AllowedCaller.Both)]
-
-        public void Execute(UnturnedPlayer caller , string[] command)
-        {
-            if (command.Length == 0)
-            {
-                if (caller is ConsolePlayer)
-                {
-                    Logger.Log("Admin , Your trying to execute the command from console , Back : to your fucking server!");
-                    return;
-                }
-                if (caller != null )
-                    TrySendBack(caller);
+                UnturnedChat.Say(Translate("null", player.DisplayName), UnturnedChat.GetColorFromName(Configuration.Instance.DeathMessagesColor, Color.green));
             }
         }
     }
-
-    }
+}
